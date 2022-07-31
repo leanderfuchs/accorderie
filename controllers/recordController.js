6921dd85225b3b3bd5382174dc77199e36a43c35
leander <leanderfuchs@gmail.com>
2022-07-31 22:27:02 +0200

    first commit

---
+++ controllers/recordController.js
@@ -0,0 +1,231 @@
+var fs = require('fs');
+var format = require('date-format');
+const { validationResult } = require('express-validator');
+const nodemailer = require('nodemailer');
+const mailjetTransport = require('nodemailer-mailjet-transport');
+
+exports.addRecord = function (req, res, body) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.render('index', { errors: errors.array(), response: req.body });
+    }
+    console.log('processing adding records ...');
+
+    const newRecord = {
+        date: format('dd/mm/yyyy hh:mm:ss', new Date()),
+        beneficiary: req.body.service_beneficiary_email,
+        provider: req.body.service_provider_email,
+        description: req.body.service_description,
+        hours: req.body.hours,
+        minutes: req.body.minutes,
+        ref: req.body.reference,
+        user_agent: req.headers['user-agent'],
+        user_ip: req.socket.remoteAddress
+    };
+
+    fs.readFile(process.env.FILES_FOLDER + 'registrations.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.sendStatus(400).send({ error: 'Désolé, une erreur de lecture est survenue. Vous pouvez recommencer ou nous appeler au 0749702058 !' })
+        }
+        console.log('file read successfully ...');
+
+        const file = JSON.parse(data);
+        file.push(newRecord);
+
+        fs.writeFile(process.env.FILES_FOLDER + 'registrations.json', JSON.stringify(file), err => {
+            if (err) {
+                console.error(err);
+                return res.sendStatus(400).render('index').send({ error: 'Désolé, une erreur d\'enregistrement est survenue. Vous pouvez recommencer ou nous appeler au 0749702058 !' })
+            }
+
+            console.log('record added successfully ...');            
+
+            sendMailToBeneficiary( 
+                newRecord.date, 
+                newRecord.beneficiary,
+                newRecord.provider,
+                newRecord.description,
+                newRecord.hours,
+                newRecord.minutes,
+                newRecord.ref
+            );
+
+            sendMailToProvider( 
+                newRecord.date, 
+                newRecord.beneficiary,
+                newRecord.provider,
+                newRecord.description,
+                newRecord.hours,
+                newRecord.minutes,
+                newRecord.ref
+            );
+
+            sendMailToLaccorderie( 
+                newRecord.date, 
+                newRecord.beneficiary,
+                newRecord.provider,
+                newRecord.description,
+                newRecord.hours,
+                newRecord.minutes,
+                newRecord.ref
+            );
+
+            sendMailTest( 
+                newRecord.date, 
+                newRecord.beneficiary,
+                newRecord.provider,
+                newRecord.description,
+                newRecord.hours,
+                newRecord.minutes,
+                newRecord.ref
+            );
+
+
+            const response = [];
+            response["service_beneficiary_email"] = '';
+            response["service_provider_email"] = '';
+            response["service_description"] = '';
+            response["hours"] = '';
+            response["minutes"] = '';
+            response["reference"] = '';
+            
+            return res.render('index', {response : response, msg: 'Votre chèque a été ajouté avec succès. Un email de confirmation a été envoyé à la boite mail des protagonistes ainsi qu\'à celle de l\'accorderie.'});
+        });
+    });
+
+    function sendMailToBeneficiary(
+        date, 
+        beneficiary,
+        provider,
+        description,
+        hours,
+        minutes,
+        ref
+    ) {
+        console.log('Sending email to beneficiary');
+
+        const transport = nodemailer.createTransport(mailjetTransport({
+            auth: {
+              apiKey: '3868dda2ada63a06e420e1b0b6aad40c',
+              apiSecret: '285a90d758a36377d1ce69b057772c2c'
+            }
+          }));
+          const mail = {
+            from: 'coeurdesbauges@accorderie.fr',
+            to: beneficiary,
+            subject: 'Nouvel enregistrement d\'un chèque temps : à ' + beneficiary + ' par ' + provider +' le '+ date ,
+
+            html: '<p>Bonjour,</p><p>Un nouveau chèque temps de <b>'+ beneficiary +'</b> rendu par <b>'+ provider +'</b> ,</p><p> pour le service: <p><b>"'+ description +'"</b></p>, le <b>'+ date +'</b>, dont la durée est de <b>'+ hours +'h. '+ minutes +'min.</b></p>'
+          };
+          
+          try {
+            const info = transport.sendMail(mail);
+            console.log(info);
+          } catch (err) {
+            console.error(err);
+          }
+    };
+
+    function sendMailToProvider(
+        date, 
+        beneficiary,
+        provider,
+        description,
+        hours,
+        minutes,
+        ref
+    ) {
+        console.log('Sending email to provider');
+
+        const transport = nodemailer.createTransport(mailjetTransport({
+            auth: {
+              apiKey: '3868dda2ada63a06e420e1b0b6aad40c',
+              apiSecret: '285a90d758a36377d1ce69b057772c2c'
+            }
+          }));
+          const mail = {
+            from: 'coeurdesbauges@accorderie.fr',
+            to: provider,
+            subject: 'Nouvel enregistrement d\'un chèque temps : à ' + beneficiary + ' par ' + provider +' le '+ date ,
+
+            html: '<p>Bonjour,</p><p>Un nouveau chèque temps de <b>'+ beneficiary +'</b> rendu par <b>'+ provider +'</b> ,</p><p> pour le service: <p><b>"'+ description +'"</b></p>, le <b>'+ date +'</b>, dont la durée est de <b>'+ hours +'h. '+ minutes +'min.</b></p>'
+          };
+          
+          try {
+            const info = transport.sendMail(mail);
+            console.log(info);
+          } catch (err) {
+            console.error(err);
+          }
+    };
+
+    function sendMailToLaccorderie(
+        date, 
+        beneficiary,
+        provider,
+        description,
+        hours,
+        minutes,
+        ref
+    ) {
+        console.log('Sending email to l\'accorderie');
+
+        const transport = nodemailer.createTransport(mailjetTransport({
+            auth: {
+              apiKey: '3868dda2ada63a06e420e1b0b6aad40c',
+              apiSecret: '285a90d758a36377d1ce69b057772c2c'
+            }
+          }));
+          const mail = {
+            from: 'coeurdesbauges@accorderie.fr',
+            to: 'coeurdesbauges@accorderie.fr',
+            subject: 'Nouvel enregistrement d\'un chèque temps : à ' + beneficiary + ' par ' + provider +' le '+ date ,
+
+            html: '<p>Bonjour,</p><p>Un nouveau chèque temps de <b>'+ beneficiary +'</b> rendu par <b>'+ provider +'</b> ,</p><p> pour le service: <p><b>"'+ description +'"</b></p>, le <b>'+ date +'</b>, dont la durée est de <b>'+ hours +'h. '+ minutes +'min.</b></p>'
+          };
+          
+          try {
+            const info = transport.sendMail(mail);
+            console.log(info);
+          } catch (err) {
+            console.error(err);
+          }
+    };
+
+    function sendMailTest(
+        date, 
+        beneficiary,
+        provider,
+        description,
+        hours,
+        minutes,
+        ref
+    ) {
+        console.log('Sending email to Leander');
+
+        const transport = nodemailer.createTransport(mailjetTransport({
+            auth: {
+              apiKey: '3868dda2ada63a06e420e1b0b6aad40c',
+              apiSecret: '285a90d758a36377d1ce69b057772c2c'
+            }
+          }));
+          const mail = {
+            from: 'leander.fuchs@webexpertbusiness.net',
+            to: 'leanderfuchs@gmail.com',
+            
+            subject: 'Nouvel enregistrement d\'un chèque temps : à ' + beneficiary + ' par ' + provider +' le '+ date ,
+
+            html: '<p>Bonjour,</p><p>Un nouveau chèque temps de <b>'+ beneficiary +'</b> rendu par <b>'+ provider +'</b> ,</p><p> pour le service: <p><b>"'+ description +'"</b></p>, le <b>'+ date +'</b>, dont la durée est de <b>'+ hours +'h. '+ minutes +'min.</b></p>'
+          };
+          
+          try {
+            const info = transport.sendMail(mail);
+            console.log(info);
+          } catch (err) {
+            console.error(err);
+          }
+    };
+
+};
---
+++ routes/index.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+const { body } = require('express-validator');
+
+const { addRecord } = require('../controllers/recordController.js');
+
+const response = [];
+response["service_beneficiary_email"] = '';
+response["service_provider_email"] = '';
+response["service_description"] = '';
+response["hours"] = '';
+response["minutes"] = '';
+response["reference"] = '';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {response});
+});
+
+/* POST home page. */
+router.post('/', 
+  body('service_beneficiary_email')
+      .notEmpty()
+      .isEmail()
+      .withMessage('L\'email du bénéficiaire est incorrect'),
+
+  body('service_provider_email')
+      .notEmpty()
+      .isEmail()
+      .withMessage('L\'email de la personne ayant rendu le service est incorrect.')
+      .custom((value,{req}) => {
+        if(value == req.body.service_beneficiary_email){
+            throw new Error("Les deux emails ne peuvent être les mêmes !");
+        } else {
+            return value;
+        };
+      }),
+
+  body('service_description')
+      .notEmpty()
+      .isLength({ min: 10 })
+      .withMessage('La description doit contenir au moins 10 caractères.'),
+
+  body('hours')
+      .isFloat({ min: 0, max: 39 })
+      .withMessage('L\'heure doit être un numéro entre 0 et 39.'),
+
+  body('minutes')
+      .notEmpty()
+      .isFloat({ min: 0, max: 59 })
+      .custom((value,{req}) => {
+        if(value == 0 && req.body.hours == 0){
+            throw new Error("Il faut une durée minimum pour le service");
+        } else {
+            return value;
+        };
+      }),
+
+  body('reference')
+      .isString()
+      .withMessage('La référence doit être composée de lettres et de chiffres.'),
+
+  addRecord
+);
+
+module.exports = router;
