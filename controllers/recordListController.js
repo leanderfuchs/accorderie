76f0508e4c673b82bc52751d93f76de713b36107
leander <leanderfuchs@gmail.com>
2022-08-15 22:33:07 +0200

    🐛 app crash when no registrations file is present on the server.

--- controllers/recordListController.js
+++ controllers/recordListController.js
@@ -8,31 +8,61 @@ exports.recordListController = function (req, res, body)
 {
     console.log('processing reading file ...');
 
-    fs.readFile( FILES_DIR + '/registrations.json', 'utf8', (err, data) => {
+    if (fs.existsSync( FILES_DIR + '/registrations.json')) 
+    {
+      fs.readFile( FILES_DIR + '/registrations.json', 'utf8', (err, data) => {
         
-      if (err) {
-        console.error(err);
-        return res.sendStatus(400).send({ error: 'Désolé, une erreur de lecture est survenue. Vous pouvez recommencer ou nous appeler au 0749702058 !' })
-      }
-
-      console.log('file read successfully ...');
-
-      var file = JSON.parse(data);
-      
-      if (!file) {
-        return res.status(404).send('Records not found')
-      }
-
-      // Fix minutes missing 0 in front of smaller numbers
-      if( file.minutes < 10) {
-        file.minutes = '0' + file.minutes;
-      }
-
-
-      file.sort(function(a, b) {
-        return b.id - a.id;
+        if (err) {
+          console.error(err);
+          return res.sendStatus(400).send({ error: 'Désolé, une erreur de lecture est survenue. Vous pouvez recommencer ou nous appeler au 0749702058 !' })
+        }
+  
+        console.log('file read successfully ...');
+  
+        var file = JSON.parse(data);
+        
+        if (!file) {
+          return res.status(404).send('Records not found')
+        }
+  
+        // Fix minutes missing 0 in front of smaller numbers
+        if( file.minutes < 10) {
+          file.minutes = '0' + file.minutes;
+        }
+  
+  
+        file.sort(function(a, b) {
+          return b.id - a.id;
+        });
+  
+        res.render('list', {data: data, records: file })
       });
+    } 
+    else 
+    {
+
+
+      fs.writeFile(FILES_DIR + '/registrations.json', '', function(err) 
+      {
+        if(err) {
+            console.log(err);
+        }
+        console.log("File saved!");
+
+        fs.appendFile( FILES_DIR + '/registrations.json', '[]', err => 
+        {
+          if (err) 
+          {
+            console.error(err);
+          }
+          console.log("File content created");
+        });
+      });
+      
+      data = [];
+      file = [];
 
       res.render('list', {data: data, records: file })
-    });
+    }
+
 };
